2ec1e6df89240058a67b4258f60e7718644e333b
kamilaalves1 <43015341+kamilaalves1@users.noreply.github.com>
2026-07-19 19:57:44 -0300

    feat(kaam-10): Implementação da funcionalidade de busca de produtos por CEP/localização.

---
+++ src/main/java/com/example/apiviacep/model/Location.java
@@ -0,0 +1,33 @@
+package com.example.apiviacep.model;
+
+public class Location {
+    private String cep;
+    private String city;
+    private String uf;
+
+    // Getters and Setters
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
