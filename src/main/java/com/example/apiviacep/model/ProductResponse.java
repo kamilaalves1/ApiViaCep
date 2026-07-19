b2596b461c0e363bb526f4b6af7552141e128ced
kamilaalves1 <43015341+kamilaalves1@users.noreply.github.com>
2026-07-19 19:57:43 -0300

    feat(kaam-10): Implementação da funcionalidade de busca de produtos por CEP/localização.

---
+++ src/main/java/com/example/apiviacep/model/ProductResponse.java
@@ -0,0 +1,35 @@
+package com.example.apiviacep.model;
+
+import java.util.List;
+
+public class ProductResponse {
+    private Location location;
+    private List<Product> products;
+    private Store nearestStore;
+
+    // Getters and Setters
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Store getNearestStore() {
+        return nearestStore;
+    }
+
+    public void setNearestStore(Store nearestStore) {
+        this.nearestStore = nearestStore;
+    }
+}
