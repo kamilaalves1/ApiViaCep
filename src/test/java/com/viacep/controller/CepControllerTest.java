cc47ad862bf5fe83ec3bb7781f483d3edd96180b
kamilaalves1 <43015341+kamilaalves1@users.noreply.github.com>
2026-07-19 20:23:09 -0300

    fix(test): corrige import MockBean para org.springframework.boot.test.mock.mockito

--- src/test/java/com/viacep/controller/CepControllerTest.java
+++ src/test/java/com/viacep/controller/CepControllerTest.java
@@ -7,7 +7,7 @@ import com.viacep.service.ViacepService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
