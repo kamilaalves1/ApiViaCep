8af38052fc9cf2cb36b8a9583670f5a1abf1ee6f
kamilaalves1 <43015341+kamilaalves1@users.noreply.github.com>
2026-07-19 23:00:48 -0300

    ** fix(arquitetura): corrigidos os imports e lógica no ProductService e adicionado tratamento de CEP no ProductController.

--- src/main/java/com/viacep/model/Store.java
+++ src/main/java/com/viacep/model/Store.java
@@ -1,11 +1,11 @@
 package com.viacep.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.AllArgsConstructor; 
+import lombok.Data; 
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Data 
+@NoArgsConstructor 
 @AllArgsConstructor
 public class Store {
     private String storeId;
