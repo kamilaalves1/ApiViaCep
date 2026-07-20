280d4920c80b88151b49c5311b6d2d42102ec53b
kamilaalves1 <43015341+kamilaalves1@users.noreply.github.com>
2026-07-19 22:55:30 -0300

    feat(kaam-18): correção de compilação com inclusão de anotações Lombok nos models e ajuste de imports no serviço e controlador.

--- src/main/java/com/viacep/model/Product.java
+++ src/main/java/com/viacep/model/Product.java
@@ -1,7 +1,14 @@
 package com.viacep.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private String productId;
     private String name;
@@ -10,61 +17,4 @@ public class Product {
     private double price;
     private List<String> availableIn;
     private String estimatedDelivery;
-
-    // Getters e Setters
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public List<String> getAvailableIn() {
-        return availableIn;
-    }
-
-    public void setAvailableIn(List<String> availableIn) {
-        this.availableIn = availableIn;
-    }
-
-    public String getEstimatedDelivery() {
-        return estimatedDelivery;
-    }
-
-    public void setEstimatedDelivery(String estimatedDelivery) {
-        this.estimatedDelivery = estimatedDelivery;
-    }
 }
